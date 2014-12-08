4f2575ca2f6426057244c3dd9dbb6a3f30661b09
Chris ter Beke <ctbeke@gmail.com>
2014-12-08 01:58:41 +0100

    super awesome core/interface dependency management

--- start.js
+++ start.js
@@ -2,14 +2,11 @@ var spawn       = require('child_process').spawn;
 var argv        = require('minimist')(process.argv.slice(2));
 var net         = require('net');
 
-if(!argv.interface) {
-    console.error('No interface given');
-    process.exit(1);
-}
-
-function readlines(stream,cb) {
+function readlines(stream,cb)
+{
     var buf = '';
-    stream.on('data',function(data) {
+    stream.on('data',function(data)
+    {
         buf += data;
         var lines = buf.split('\n');
         buf = lines.pop();
@@ -18,23 +15,54 @@ function readlines(stream,cb) {
     return stream;
 }
 
-if(argv.simulator) {
+if(!argv.interface)
+{
+    console.error('No interface given');
+    process.exit(1);
+}
+else
+{
+	var interfaceConfig = require('./' + argv.interface + '/man_config.json');
+	var coreConfig = require('./printspot-core/config/core.json');
+
+	if(!interfaceConfig.dependencies['printspot-core'])
+	{
+		console.error('Interface config has no printspot-core dependency version listed');
+		process.exit(1);
+	}
+	else if(!coreConfig.version.number)
+	{
+		console.error('Core config has no verion number listed');
+		process.exit(1);
+	}
+	else if(coreConfig.version.number != interfaceConfig.dependencies['printspot-core'])
+	{
+		console.error('Core version number is not compatible with interface printspot-core dependency version number');
+		process.exit(1);
+	}
+}
+
+if(argv.simulator)
+{
 	var client = spawn('node', ['index.js'], {cwd: 'printspot-qclient-simulator', stdio: 'pipe'});
 }
 
-if(argv.slicer) {
+if(argv.slicer)
+{
 	var katana = spawn('node', ['index.js'], {cwd: 'printspot-katana-simulator', stdio: 'pipe'});
 }
 
 var core = spawn('node', ['index.js','--dev'], {cwd: 'printspot-core', stdio: 'pipe'});
 var manufacturer = spawn('node', ['index.js'], {cwd: argv.interface, stdio: 'pipe'});
 
-if(argv.simulator) {
+if(argv.simulator)
+{
     client.on('exit',function(code) { console.error('client exited', code); });
 	client.on('error',function(err) { console.log('client error',err); });
 }
 
-if(argv.slicer) {
+if(argv.slicer)
+{
 	katana.on('exit',function(code) { console.error('katana exited', code); });
 	katana.on('error',function(err) { console.log('katana error',err); });
 }
@@ -45,15 +73,18 @@ core.on('error',function(err) { console.log('core error',err); });
 manufacturer.on('exit',function(code) { console.error('manufacturer exited', code); });
 manufacturer.on('error',function(err) { console.log('manufacturer error',err); });
 
-if(argv.dev) {
+if(argv.dev)
+{
     console.log('running in dev mode');
 
-    if(argv.simulator) {
+    if(argv.simulator)
+    {
 		readlines(client.stdout, console.log.bind(null,'client:'));
 		readlines(client.stderr, console.error.bind(null,'client:'));
 	}
 
-	if(argv.slicer) {
+	if(argv.slicer)
+	{
 		readlines(katana.stdout, console.log.bind(null,'katana:'));
 		readlines(katana.stderr, console.error.bind(null,'katana:'));
 	}
