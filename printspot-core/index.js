930c76764357513afd25a7443821a7dac6e93beb
Bob van der Linden <bobvanderlinden@gmail.com>
2014-12-09 13:19:06 +0100

    use config.local.host from configuration for Access-Control

--- printspot-core/index.js
+++ printspot-core/index.js
@@ -71,7 +71,7 @@ global.app.use(session(
 
 global.app.all('/*', function(req, res, next)
 {
-	res.header("Access-Control-Allow-Origin", "http://localhost:1336");
+	res.header("Access-Control-Allow-Origin", global.config.local.host+":1336");
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Credentials", "true");
