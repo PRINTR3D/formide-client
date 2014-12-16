ebd2d90c12ffeac0d924b7037d29116c2a1bb7bd
Chris ter Beke <ctbeke@gmail.com>
2014-12-16 20:31:05 +0100

    katana simulator now simulating slicing as well

--- printspot-katana-simulator/index.js
+++ printspot-katana-simulator/index.js
@@ -2,16 +2,37 @@ process.title = 'printspot-katana-simulator';
 
 var net = require('net');
 
-var server = net.createServer(function(client) {
-
-	client.on('data', function(data) {
+var server = net.createServer(function(client)
+{
+	client.on('data', function(data)
+	{
 		console.log(data.toString());
+		data = JSON.parse(data);
+
+		var json = {
+			"status": 200,
+			"data": {
+				"materials":
+				[
+					{
+						"amount": 1157,
+						"extruder": 0
+					}
+				],
+				"time": 2598,
+				"gcode": "GCODE_FILE_LOCATION",
+				"responseID": data.data.responseID
+			}
+		}
+
+		client.write(JSON.stringify(json));
 	});
 
 	console.log('katana simulator running');
 });
 
-server.listen(1339, function() { //'listening' listener
+server.listen(1339, function()
+{
   	address = server.address();
   	console.log("opened server on %j", address);
 });
