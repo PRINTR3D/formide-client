f59251b193c946c8b2fa60df839747d683cd0809
Bouke Regnerus <bouke@regner.us>
2015-01-08 14:59:01 +0100

    Added Hostname

--- core/utils/config.js
+++ core/utils/config.js
@@ -19,7 +19,7 @@ module.exports = function()
 	var config = Config(
 		__dirname + '/..',
 		{
-			development: ['chris.local', 'bouke.local', 'wlan228086.mobiel.utwente.nl'],
+			development: ['chris.local', 'bouke.local', 'bouke', 'wlan228086.mobiel.utwente.nl'],
 			production: ['raspberrypi']
 		}
 	);
