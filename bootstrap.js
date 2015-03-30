99f5ba832b2e828b8fc1a84169d61276c9a49661
Chris ter Beke <ctbeke@gmail.com>
2015-03-30 15:45:35 +0200

    removed store from bootstrap

--- bootstrap.js
+++ bootstrap.js
@@ -26,7 +26,6 @@ getMac.getMac(function(err, macAddress)
 	FormideOS.register('core.http');
 	FormideOS.register('core.process');
 	FormideOS.register('core.db');
-	FormideOS.register('core.store', FormideOS.config.get('store'));
 	FormideOS.register('core.auth');
 	FormideOS.register('core.websocket');
 	FormideOS.register('core.device');
