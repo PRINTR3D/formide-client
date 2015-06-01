4f328bd2e705418c5574c835d42db91bf3d5290e
ChrisTerBeke <ctbeke@gmail.com>
2015-06-02 00:29:26 +0200

    cleanup

--- tests/process.js
+++
@@ -1,8 +0,0 @@
-Printspot = require('../Printspot.js')();
-Printspot.register('process').init();
-
-exports['init'] = function( test )
-{
-	test.ok(typeof Printspot.manager('process').init === 'function', 'Init function not found');
-	test.done();
-};
