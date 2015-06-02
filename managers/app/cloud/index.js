2327a0e13a9bbe42891e8cb9dce1a9697728c3cc
ChrisTerBeke <ctbeke@gmail.com>
2015-06-02 11:48:06 +0200

    added comments to cloud manager

--- managers/app/cloud/index.js
+++ managers/app/cloud/index.js
@@ -22,11 +22,17 @@ module.exports =
 	cloud: {},
 	local: [],
 
+	/*
+	 * Init for cloud manager
+	 */
 	init: function( config )
 	{
+		// init cloud with new socket io client to online cloud url
 		this.cloud = socket( config.url );
 
+		// when connecting to cloud
 		this.cloud.on('connect', function() {
+			// authenticate formideos based on mac address and api token, also sends permissions for faster blocking via cloud
 			this.cloud.emit('authenticate', {
 				type: 'client',
 				mac: FormideOS.macAddress,
@@ -36,36 +42,46 @@ module.exports =
 			FormideOS.manager('debug').log('Cloud connected');
 		}.bind(this));
 
+		// on http proxy request
 		this.cloud.on('http', function(data, callback) {
 			FormideOS.manager('debug').log('Cloud http call: ' + data.manager + '/' + data.function);
+			// call http function
 			this.http(data, function(response) {
 				callback(response);
 			});
 		}.bind(this));
 
+		// on ws proxy request
 		this.cloud.on('listen', function(data, callback) {
 			FormideOS.manager('debug').log('Cloud ws listen: ' + data.manager + '/' + data.channel);
+			// call listen function
 			this.listen(data, function(response) {
 				callback(response);
 			});
 		}.bind(this));
 
+		// emit ws to cloud
 		this.cloud.on('emit', function(data, callback) {
 			FormideOS.manager('debug').log('Cloud ws emit: ' + data.manager + '/' + data.channel);
+			// call emit function
 			this.emit(data);
 		}.bind(this));
 
+		// when disconnecting
 		this.cloud.on('disconnect', function() {
 			FormideOS.manager('debug').log('Cloud diconnected');
 		});
 	},
 
+	/*
+	 * Handles HTTP proxy function calls from cloud connection, calls own local http api after reconstructing HTTP request
+	 */
 	http: function(data, callback) {
 		request({
 			method: data.method,
 			uri: 'http://127.0.0.1:' + FormideOS.manager('core.http').server.server.address().port + '/api/' + data.manager + '/' + data.function,
 			auth: {
-				bearer: data.token
+				bearer: data.token // add cloud api key to authorise to local HTTP api
 			},
 			form: data.data || {}
 		}, function( error, response, body ) {
@@ -73,6 +89,9 @@ module.exports =
 		});
 	},
 
+	/*
+	 * Handles WS proxy call from cloud connection, calls own local ws server and relays calls 
+	 */
 	listen: function(data, callback) {
 		var self = this;
 		if(!this.local[data.manager]) {
@@ -83,6 +102,9 @@ module.exports =
 		});
 	},
 
+	/*
+	 * Handles WS emits to proxy (printer status/logs/slicer status), relays local emits to cloud
+	 */
 	emit: function(data) {
 		var self = this;
 		if(!this.local[data.manager]) {
