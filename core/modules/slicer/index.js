6ac274aa1681f5cf322756a8c80a8dd0dc6291e0
ChrisTerBeke <ctbeke@gmail.com>
2015-09-10 11:08:00 +0200

    add sliceprofile update function, reference file for katana

--- core/modules/slicer/index.js
+++ core/modules/slicer/index.js
@@ -4,18 +4,21 @@
  */
 
 // dependencies
-var spawn 	= require('child_process').spawn;
-var fs 		= require('fs');
-var net 	= require('net');
-var uuid 	= require('node-uuid');
+var spawn 		= require('child_process').spawn;
+var fs 			= require('fs');
+var net 		= require('net');
+var uuid 		= require('node-uuid');
+var deepExtend	= require("deep-extend");
 
 module.exports = {
 
 	katana: null,
+	config: {},
 
 	init: function(config) {
 		this.config = config;
 
+/*
 		if(process.platform == 'darwin') {
 			this.katana	= require(FormideOS.appRoot + 'bin/osx/katana');
 			FormideOS.debug.log('Binded katana in osx/katana');
@@ -24,6 +27,7 @@ module.exports = {
 			this.katana	= require(FormideOS.appRoot + 'bin/rpi/katana');
 			FormideOS.debug.log('Binded katana in rpi/katana');
 		}
+*/
 	},
 
 	// custom functions
@@ -118,163 +122,248 @@ module.exports = {
 		});
 	},
 	
+	checkSliceprofile: function(sliceprofileId, callback) {
+		var reference = require(FormideOS.appRoot + "bin/katana-" + this.config.version + ".json");
+		FormideOS.module('db').db.Sliceprofile.findOne({ _id: sliceprofileId }).lean().exec(function(err, sliceprofile) {
+			if (err) return callback("Error getting sliceprofile: " + err.message);
+			
+			var settings = sliceprofile.settings;
+			
+			if (reference.version !== sliceprofile.version) {
+				FormideOS.debug.log("Used sliceprofile is not the current version, updating to latest parameter set...");
+				sliceprofile.version = reference.version; // update version number
+			}
+			else {
+				FormideOS.debug.log("Used sliceprofile is the current version, checking parameter set anyway...")
+			}
+			
+			for (var i in reference.sliceProfile) {
+				
+				// 1: Check if sliceprofileObject is in current sliceprofile. If not, create empty object and continue.
+				if (typeof settings[i] === "undefined") {
+					settings[i] = {};
+					console.log("Fixed missing object: " + i);
+				}
+				
+				// 2: Check all fields in a sliceprofileObject. If not there, add it with the given default value.
+				for (var j in reference.sliceProfile[i]) {
+					if (typeof settings[i][j] === "undefined") {
+						settings[i][j] = reference.sliceProfile[i][j].default;
+						console.log("Fixed missing parameter: " + i + " - " + j);
+					}
+				}
+				
+				// 3: Check all fields in a sliceprofileObject for their values and fix invalid values.
+				for (var j in reference.sliceProfile[i]) {
+					if (typeof settings[i][j] !== "undefined") {
+						if (reference.sliceProfile[i][j].minvalue && reference.sliceProfile[i][j].minvalue > settings[i][j]) {
+							settings[i][j] = reference.sliceProfile[i][j].minvalue;
+							console.log("Fixed out of bounds parameter: " + i + " - " + j);
+						}
+						if (reference.sliceProfile[i][j].maxvalue && reference.sliceProfile[i][j].maxvalue < settings[i][j]) {
+							settings[i][j] = reference.sliceProfile[i][j].maxvalue;
+							console.log("Fixed out of bounds parameter: " + i + " - " + j);
+						}
+					}
+				}
+			}
+			
+			// 4: Check all depricated fields and remove them from sliceprofile
+			for (var i in reference.deprecated) {
+				if (typeof settings[i] !== "undefined") {
+					for (var j in reference.deprecated[i]) {
+						if (typeof settings[i][reference.deprecated[i][j]] !== "undefined") {
+							delete settings[i][reference.deprecated[i][j]];
+							console.log("Fixed deprecated parameter: " + i + " - " + reference.deprecated[i][j]);
+						}
+					}
+				}
+			}
+			
+			// 5: Save sliceprofile and use in next step.
+			sliceprofile.settings = settings;
+			
+			FormideOS.module('db').db.Sliceprofile.update({ _id: sliceprofile._id }, { settings: settings, version: sliceprofile.version }, function(err, update) {
+				if (err) return callback("Error updating sliceprofile: " + err.message);
+				return callback(null, sliceprofile);
+			});
+		});
+	},
+	
 	createSliceRequest: function(printjobId, callback) {
 		
+		var self = this;
+		
 		// creates a slice request from a printjob database entry
-		FormideOS.module('db').db.Printjob.findOne({ _id: printjobId }).lean().populate('modelfiles sliceprofile materials printer').exec(function(err, printjob) {
-		
-			var slicerequest = printjob.sliceprofile.settings;
+		FormideOS.module('db').db.Printjob.findOne({ _id: printjobId }).lean().populate('modelfiles materials printer').exec(function(err, printjob) {
+			if (err) return callback("Error getting printjob: " + err.message);
+			if (printjob.printer === null) return callback("Error getting printjob printer");
+			if (printjob.modelfiles.length < 1) return callback("Error getting printjob modelfiles");
+			if (printjob.materials.length < 1) return callback("Error getting printjob materials");
 			
-			slicerequest.bed = {
-				xlength: printjob.printer.bed.x * 1000,
-				ylength: printjob.printer.bed.y * 1000,
-				zlength: printjob.printer.bed.z * 1000,
-				temperature: 0,
-				firstLayersTemperature: 0
-			};
-		
-			if(printjob.sliceSettings) {
-				if(printjob.sliceSettings.brim) {
-					if(printjob.sliceSettings.brim.use === false) {
-						delete slicerequest.brim;
-					}
-					else {
-						if(printjob.sliceSettings.brim.extruder != undefined) {
-							slicerequest.brim.extruder = printjob.sliceSettings.brim.extruder;
-						}
-						else {
-							return callback("No extruder given for brim");
-						}
-					}
-				}
-				else {
-					return callback("No brim settings given");
-				}
-				
-				if(printjob.sliceSettings.raft) {
-					if(printjob.sliceSettings.raft.use === false) {
-						delete slicerequest.raft;
-					}
-					else {
-						if(printjob.sliceSettings.raft.extruder != undefined) {
-							slicerequest.raft.extruder = printjob.sliceSettings.raft.extruder;
-						}
-						else {
-							return callback("No extruder given for raft");
-						}
-					}
-				}
-				else {
-					return callback("No raft settings given");
-				}
-				
-				if(printjob.sliceSettings.support) {
-					if(printjob.sliceSettings.support.use === false) {
-						delete slicerequest.support;
-					}
-					else {
-						if(printjob.sliceSettings.support.extruder != undefined) {
-							slicerequest.support.extruder = printjob.sliceSettings.support.extruder;
-						}
-						else {
-							return callback("No extruder given for support");
-						}
-					}
-				}
-				else {
-					return callback("No support settings given");
-				}
-				
-				if(printjob.sliceSettings.skirt) {
-					if(printjob.sliceSettings.skirt.use === false) {
-						delete slicerequest.skirt;
-					}
-					else {
-						if(printjob.sliceSettings.skirt.extruder != undefined) {
-							slicerequest.skirt.extruder = printjob.sliceSettings.skirt.extruder;
-						}
-						else {
-							return callback("No extruder given for skirt");
-						}
-					}
-				}
-				else {
-					return callback("No skirt settings given");
-				}
-				
-				if(printjob.sliceSettings.fan) {
-					if(printjob.sliceSettings.fan.use === false) {
-						delete slicerequest.fan;
-					}
-					else {
-						if(printjob.sliceSettings.fan.speed != undefined) {
-							slicerequest.fan.speed = printjob.sliceSettings.fan.speed;
-						}
-						else {
-							return callback("No speed given for fan");
-						}
-					}
-				}
-				else {
-					return callback("No fan settings given");
-				}
-				
-				if(printjob.sliceSettings.bed) {
-					if(printjob.sliceSettings.bed.use !== false) {
-						if(printjob.sliceSettings.bed.temperature != undefined) {
-							slicerequest.bed.temperature = printjob.sliceSettings.bed.temperature;
-						}
-						else {
-							return callback("No bed temperature given");
-						}
-						if(printjob.sliceSettings.bed.firstLayersTemperature != undefined) {
-							slicerequest.bed.firstLayersTemperature = printjob.sliceSettings.bed.firstLayersTemperature;
-						}
-						else {
-							return callback("No bed firstLayersTemperature given");
-						}
-					}
-				}
-			}
+			this.checkSliceprofile(printjob.sliceprofile, function(err, fixedSliceprofile) {
+				if (err) return callback("Error checking sliceprofile: " + err.message);	
 			
-			slicerequest.model = [];
-			for(var i in printjob.modelfiles) {
-				var model = printjob.modelfiles[i];
-				var extruderForModel = 0;
-				if(printjob.sliceSettings.modelfiles) {
-					printjob.sliceSettings.modelfiles[i].extruder;
-				}
-				slicerequest.model.push({
-					hash: model.hash,
-					bucketIn: FormideOS.config.get('app.storageDir') + FormideOS.config.get("paths.modelfile"),
-					x: 100, 		// TODO: set user specified position
-					y: 100, 		// TODO: set user specified position
-					z: 0, 			// TODO: set user specified position
-					extruder: extruderForModel
-				});
-			}
+				var slicerequest = fixedSliceprofile.settings;
+				
+				slicerequest.bed = {
+					xlength: printjob.printer.bed.x * 1000, // input is in mm
+					ylength: printjob.printer.bed.y * 1000, // input is in mm
+					zlength: printjob.printer.bed.z * 1000, // input is in mm
+					temperature: 0,
+					firstLayersTemperature: 0
+				};
 			
-			slicerequest.extruders = [];
-			for(var i in printjob.printer.extruders) {
-				var extruder = printjob.printer.extruders[i];
-				var material = printjob.materials[i];
-				if(material) {
-					slicerequest.extruders.push({
-						name: extruder.name,
-						material: material.type,
-						nozzleSize: extruder.nozzleSize,
-						temperature: material.temperature,
-						firstLayersTemperature: material.firstLayersTemperature,
-						filamentDiameter: material.filamentDiameter,
-						feedrate: material.feedrate
+				if(printjob.sliceSettings) {
+					if(printjob.sliceSettings.brim) {
+						if(printjob.sliceSettings.brim.use === false) {
+							delete slicerequest.brim;
+						}
+						else {
+							if(printjob.sliceSettings.brim.extruder != undefined) {
+								slicerequest.brim.extruder = printjob.sliceSettings.brim.extruder;
+							}
+							else {
+								return callback("No extruder given for brim");
+							}
+						}
+					}
+					else {
+						return callback("No brim settings given");
+					}
+					
+					if(printjob.sliceSettings.raft) {
+						if(printjob.sliceSettings.raft.use === false) {
+							delete slicerequest.raft;
+						}
+						else {
+							if(printjob.sliceSettings.raft.extruder != undefined) {
+								slicerequest.raft.extruder = printjob.sliceSettings.raft.extruder;
+							}
+							else {
+								return callback("No extruder given for raft");
+							}
+						}
+					}
+					else {
+						return callback("No raft settings given");
+					}
+					
+					if(printjob.sliceSettings.support) {
+						if(printjob.sliceSettings.support.use === false) {
+							delete slicerequest.support;
+						}
+						else {
+							if(printjob.sliceSettings.support.extruder != undefined) {
+								slicerequest.support.extruder = printjob.sliceSettings.support.extruder;
+							}
+							else {
+								return callback("No extruder given for support");
+							}
+						}
+					}
+					else {
+						return callback("No support settings given");
+					}
+					
+					if(printjob.sliceSettings.skirt) {
+						if(printjob.sliceSettings.skirt.use === false) {
+							delete slicerequest.skirt;
+						}
+						else {
+							if(printjob.sliceSettings.skirt.extruder != undefined) {
+								slicerequest.skirt.extruder = printjob.sliceSettings.skirt.extruder;
+							}
+							else {
+								return callback("No extruder given for skirt");
+							}
+						}
+					}
+					else {
+						return callback("No skirt settings given");
+					}
+					
+					if(printjob.sliceSettings.fan) {
+						if(printjob.sliceSettings.fan.use === false) {
+							delete slicerequest.fan;
+						}
+					}
+					else {
+						return callback("No fan settings given");
+					}
+					
+					if(printjob.sliceSettings.bed) {
+						if(printjob.sliceSettings.bed.use !== false) {
+							if(printjob.sliceSettings.bed.temperature != undefined) {
+								slicerequest.bed.temperature = printjob.sliceSettings.bed.temperature;
+							}
+							else {
+								return callback("No bed temperature given");
+							}
+							if(printjob.sliceSettings.bed.firstLayersTemperature != undefined) {
+								slicerequest.bed.firstLayersTemperature = printjob.sliceSettings.bed.firstLayersTemperature;
+							}
+							else {
+								return callback("No bed firstLayersTemperature given");
+							}
+						}
+					}
+					
+					// override slice parameters
+					if(printjob.sliceSettings.override) {
+						deepExtend(slicerequest, printjob.sliceSettings.override);
+					}
+				}
+				
+				slicerequest.model = [];
+				for(var i in printjob.modelfiles) {
+					var model = printjob.modelfiles[i];
+					var extruderForModel = 0;
+					var xPos = 0;
+					var yPos = 0;
+					var zPos = 0;
+					
+					if(printjob.sliceSettings.modelfiles) {
+						extruderForModel = printjob.sliceSettings.modelfiles[i].extruder;
+						xPos = printjob.sliceSettings.modelfiles[i].root.x * 1000; // input is in mm
+						yPos = printjob.sliceSettings.modelfiles[i].root.z * 1000; // three.js uses y as height!
+						zPos = printjob.sliceSettings.modelfiles[i].root.y * 1000; // three.js uses y as height!
+					}
+					
+					slicerequest.model.push({
+						hash: model.hash,
+						bucketIn: process.env.KATANA_BUCKETIN,
+						x: xPos,
+						y: yPos,
+						z: zPos,
+						extruder: extruderForModel,
+						settings: 0
 					});
 				}
-			}
-			
-			slicerequest.bucketOut = FormideOS.config.get('app.storageDir') + FormideOS.config.get("paths.gcode");
-			slicerequest.responseID = printjob._id.toString();
-		
-			callback(null, slicerequest);
+				
+				slicerequest.extruders = [];
+				for(var i in printjob.printer.extruders) {
+					var extruder = printjob.printer.extruders[i];
+					var material = printjob.materials[i];
+					if(material) {
+						slicerequest.extruders.push({
+							name: extruder.name,
+							material: material.type,
+							nozzleSize: extruder.nozzleSize,
+							temperature: material.temperature,
+							firstLayersTemperature: material.firstLayersTemperature,
+							filamentDiameter: material.filamentDiameter,
+							feedrate: material.feedrate
+						});
+					}
+				}
+				
+				slicerequest.bucketOut = FormideOS.config.get('app.storageDir') + FormideOS.config.get("paths.gcode");
+				slicerequest.responseID = printjob._id.toString();
+				slicerequest.version = fixedSliceprofile.version;
+				
+				callback(null, slicerequest);
+			});
 		});
 	}
 }
