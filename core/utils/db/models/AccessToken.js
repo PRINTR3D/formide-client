72dd8a2a272a119c687993abd5e8934e02685015
ChrisTerBeke <ctbeke@gmail.com>
2015-11-06 11:05:21 +0100

    add db files

---
+++ core/utils/db/models/AccessToken.js
@@ -0,0 +1,47 @@
+/*
+ *	This code was created for Printr B.V. It is open source under the formideos-client package.
+ *	Copyright (c) 2015, All rights reserved, http://printr.nl
+ */
+
+var uuid = require('node-uuid');
+
+module.exports = function(Waterline) {
+
+	var AccessToken = Waterline.Collection.extend({
+	
+		identity: 'accesstoken',
+		
+		connection: 'default',
+		
+		attributes: {
+			
+			// authorized user
+			user: {
+				model: 'User'
+			},
+			
+			// the actual access token
+			token: {
+				type: 'string'
+			},
+			
+			permissions: {
+				type: 'array',
+				defaultsTo: []
+			},
+			
+			sessionOrigin: {
+				type: 'string',
+				required: true
+			}
+		},
+		
+		// hash password before saving user to database
+		beforeCreate: function (user, next) {
+			values.token = uuid.v4();
+			next();
+		}
+	});
+	
+	return AccessToken;
+}
---
+++ core/utils/db/models/User.js
@@ -0,0 +1,84 @@
+/*
+ *	This code was created for Printr B.V. It is open source under the formideos-client package.
+ *	Copyright (c) 2015, All rights reserved, http://printr.nl
+ */
+
+var bcrypt = require('bcrypt');
+
+module.exports = function(Waterline) {
+
+	var User = Waterline.Collection.extend({
+	
+		identity: 'user',
+		
+		connection: 'default',
+		
+		attributes: {
+			
+			email: {
+				type: 'string',
+				email: true,
+				required: true
+			},
+			
+			password: {
+				type: 'string',
+				protected: true
+			},
+			
+			cloudConnectionToken: {
+				type: 'string'
+			},
+			
+			isAdmin: {
+				type: 'boolean',
+				defaultsTo: false
+			},
+			
+			isOwner: {
+				type: 'boolean',
+				defaultsTo: false
+			}
+		},
+		
+		// hash password before saving user to database
+		beforeCreate: function (user, next) {
+			if (user.password) {
+				bcrypt.genSalt(10, function(err, salt) {
+					bcrypt.hash(user.password, salt, function(err, hash) {
+						if (err) {
+							sails.log(err);
+							next(err);
+						}
+						user.password = hash;
+						next();
+					})
+				})
+			}
+			else {
+				next();
+			}
+		},
+		
+		// hash password before saving to database
+		beforeUpdate: function (user, next) {
+			if (user.password) {
+				bcrypt.genSalt(10, function(err, salt) {
+					bcrypt.hash(user.password, salt, function(err, hash) {
+						if (err) {
+							sails.log(err);
+							next(err);
+						}
+						user.password = hash;
+						next();
+					})
+				})
+			}
+			else {
+				next();
+			}
+		}
+	});
+	
+	return User;
+}
