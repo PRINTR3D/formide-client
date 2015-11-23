a366da45e34a8329bf6b62168b43363b47c877c2
ChrisTerBeke <ctbeke@gmail.com>
2015-11-23 17:33:48 +0100

    update pagination response

--- core/utils/http/responses/paginate.js
+++ core/utils/http/responses/paginate.js
@@ -1,53 +1,58 @@
 /**
- * 200 (OK) Response
+ * 200 (paginate) Response
  *
  * Usage:
- * return res.ok();
- * return res.ok(data);
- * return res.ok(data, 'auth/login');
+ * return res.paginate(data);
+ * return res.paginate(data, {});
  *
  * @param  {Object} data
  * @param  {String|Object} options
  *          - pass string to render specified view
  */
 
-module.exports = function sendOK (data, options) {
+module.exports = function sendPaginate (data, options) {
 
-	// Get access to `req`, `res`, & `sails`
 	var req = this.req;
 	var res = this.res;
-	var sails = req._sails;
-	
-	// set defaults for options
-	options = options || {
-	  prev: true,
-	  next: true
-	};
-	
+
+	options = options || {};
+	options.count = options.count || 0;
+
+	FormideOS.log.silly('res.paginate() :: Sending 200 (paginate) response');
+
 	// Set status code
 	res.status(200);
-	
+
 	var responseLinkHeaders = [];
-	
-	if (options.next) {
-		responseLinkHeaders.push('<' + process.env.ROOTURL + '/auth/sessions?offset=' + (parseInt(req.pagination.offset) + parseInt(req.pagination.limit)) + '&limit=' + req.pagination.limit + '>; rel="next"');
+	var prevOffset = (parseInt(req.pagination.offset, 10) - parseInt(req.pagination.limit, 10));
+	var nextOffset = (parseInt(req.pagination.offset, 10) + parseInt(req.pagination.limit, 10));
+	var lastPageOffset = 0;
+
+	// get last page by taking modulus of limit and total count
+	if (options.count !== 0) {
+		lastPageOffset = (parseInt(req.pagination.limit, 10) % options.count) * req.pagination.limit;
 	}
-	
-	if (options.prev) {
-		responseLinkHeaders.push('<' + process.env.ROOTURL + '/auth/sessions?offset=' + (parseInt(req.pagination.offset) - parseInt(req.pagination.limit)) + '&limit=' + req.pagination.limit + '>; rel="next"');
+
+	// only add prev item when offset is 0 or larger
+	if (prevOffset > -1) {
+		responseLinkHeaders.push('<' + process.env.ROOTURL + req.route.path + '?offset=' + prevOffset + '&limit=' + req.pagination.limit + '>; rel=\x27prev\x27');
 	}
-	
-	if (options.first) {
-	//responseLinkHeaders.push('<' + process.env.ROOTURL + '/auth/sessions?offset=' + (req.pagination.offset - req.pagination.limit) + '&limit=' + req.pagination.limit + '>; rel="next"');
+
+	// only add next link when page contains maximum number of items
+	if (nextOffset < options.count) {
+		responseLinkHeaders.push('<' + process.env.ROOTURL + req.route.path + '?offset=' + nextOffset + '&limit=' + req.pagination.limit + '>; rel=\x27next\x27');
 	}
-	
-	if (options.last) {
-	//responseLinkHeaders.push('<' + process.env.ROOTURL + '/auth/sessions?offset=' + (req.pagination.offset - req.pagination.limit) + '&limit=' + req.pagination.limit + '>; rel="next"');
-	}
-	
+
+	// set first and last link headers
+	responseLinkHeaders.push('<' + process.env.ROOTURL + req.route.path + '?offset=0&limit=' + req.pagination.limit + '>; rel=\x27first\x27');
+	responseLinkHeaders.push('<' + process.env.ROOTURL + req.route.path + '?offset=' + lastPageOffset + '&limit=' + req.pagination.limit + '>; rel=\x27last\x27');
+
 	// If appropriate, serve data as JSON(P) with appropriate Link header
-	res.set("Link", responseLinkHeaders.join(','));
-	
+	res.set('Link', responseLinkHeaders.join(','));
+
+	// Set content-range header
+	res.set('Content-Range', req.pagination.offset + '-' + nextOffset + '/' + options.count);
+
 	// return response
 	return res.jsonx(data);
 };
