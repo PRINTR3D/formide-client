b099563633a8eb046186a8139ba4ae0cadf373a3
ChrisTerBeke <ctbeke@gmail.com>
2015-11-30 16:27:58 +0100

    actually send success callback when connecting to events

--- core/utils/websocket/index.js
+++ core/utils/websocket/index.js
@@ -2,7 +2,7 @@
  *	This code was created for Printr B.V. It is open source under the formideos-client package.
  *	Copyright (c) 2015, All rights reserved, http://printr.nl
  */
- 
+
 /*
  *	Websocket server. All modules can add namespaced to it. Permissions handled automatically by
  *	FormideOS.
@@ -15,7 +15,7 @@ var ws = require("nodejs-websocket");
 module.exports = {
 
 	init: function() {
-		
+
 		// base websocket server to LCD display notifications and events
 		var server = ws.createServer(function (conn) {
 			FormideOS.log("Native UI connected");
@@ -25,7 +25,7 @@ module.exports = {
 					if (data.channel === "authenticate") {
 						authenticate(data.data.accessToken, function(err, accessToken) {
 							if (err) console.log(err);
-							
+
 							conn.sendText(JSON.stringify({
 								channel: "authenticate",
 								data: {
@@ -34,7 +34,7 @@ module.exports = {
 									notification: false
 								}
 							}))
-							
+
 							FormideOS.events.onAny(function(data) {
 								console.log(this.event);
 								data = data || {};
@@ -51,22 +51,22 @@ module.exports = {
 					console.log(e);
 				}
 			});
-			
+
 		    conn.on("close", function (code, reason) {
 		        FormideOS.log("Native UI disconnected: " + reason);
 		    });
-		    
+
 		    conn.on("error", function (err) {
 			    FormideOS.events.offAny(function (){});
 				FormideOS.log.error(err.message);
 		    });
-		    
+
 		}).listen(3001)
-		
-		
-		
+
+
+
 		var socketio = io.listen(FormideOS.http.server);
-		
+
 		// emit all system events
 		socketio.on('connection', function(socket) {
 			socket.on('authenticate', function(data, callback) {
@@ -75,27 +75,32 @@ module.exports = {
 						callback({ success: false, message: err.message });
 						return socket.disconnect();
 					}
-					
+
 					if (!accessToken) {
 						callback({ success: false, message: "Access token not found. Please provide a valid access token." });
 						return socket.disconnect();
 					}
-					
+
 					FormideOS.events.onAny(function(data) {
 						data = data || {};
 						data.device = "LOCAL";
 						socket.emit(this.event, data);
 					});
+
+                    callback({
+                        success: true,
+                        message: 'Authentication successful'
+                    });
 				});
 			});
 		});
-		
+
 		function authenticate(token, callback) {
 			FormideOS.db.AccessToken.findOne({ token: token }).exec(function(err, accessToken) {
 				return callback(err, accessToken);
 			});
 		}
-		
+
 		return socketio;
 	}
 }
