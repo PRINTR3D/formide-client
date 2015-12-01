514ca3855d1338e35190faf2ded6f74ef28be9f0
ChrisTerBeke <ctbeke@gmail.com>
2015-12-01 12:18:53 +0100

    add

---
+++ core/utils/db/models_old/userfile.js
@@ -0,0 +1,74 @@
+/*
+ *	This code was created for Printr B.V. It is open source under the formideos-client package.
+ *	Copyright (c) 2015, All rights reserved, http://printr.nl
+ */
+
+module.exports = function(Waterline) {
+
+	var UserFile = Waterline.Collection.extend({
+
+		identity: 'userfile',
+
+		connection: 'default',
+
+		attributes: {
+
+			createdBy: {
+				model: 'User',
+				required: true
+			},
+
+			hash: {
+				type: 'string',
+				required: true
+			},
+
+			filename: {
+				type: 'string',
+				required: true
+			},
+
+			folder: {
+				type: 'string',
+				defaultsTo: '/'
+			},
+
+			prettyname: {
+				type: 'string',
+				required: true
+			},
+
+			filetype: {
+				type: 'string',
+				required: true,
+				enum: ['text/stl', 'text/gcode']
+			},
+
+			filesize: {
+				type: 'int',
+				required: true
+			},
+
+/*
+			images: {
+				collection: 'Image'
+			},
+*/
+
+			tags: {
+				type: 'array'
+			},
+
+			description: {
+				type: 'string'
+			},
+
+			printJobs: {
+				collection: 'printjob',
+				via: 'files'
+			}
+		}
+	});
+
+	return UserFile;
+}
