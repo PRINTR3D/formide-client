17845668742958fbb2ac4e8f79becf21b43f6450
ChrisTerBeke <ctbeke@gmail.com>
2015-12-15 23:24:27 +0100

    use bcryptjs instead of bcrypt because the package fails to install too often

--- core/utils/http/index.js
+++ core/utils/http/index.js
@@ -2,7 +2,7 @@
  *	This code was created for Printr B.V. It is open source under the formideos-client package.
  *	Copyright (c) 2015, All rights reserved, http://printr.nl
  */
- 
+
 /*
  *	HTTP server for FormideOS. All modules can add a sub-app to this express webserver. Permissions
  *	are added automatically by module namespace. We use passport for authentication with local login.
@@ -25,18 +25,18 @@ var isUserMiddleware		= require('./middleware/isUser');
 var isAdminMiddleware		= require('./middleware/isAdmin');
 var passwordHash 			= require('password-hash');
 var bearerToken 			= require('express-bearer-token');
-var bcrypt					= require('bcrypt');
+var bcrypt					= require('bcryptjs');
 
 module.exports = {
-	
+
 	app: null,
 	httpServer: null,
 	passport: null,
-	
+
 	init: function() {
 		this.setupApp();
 		this.setupPassport();
-		
+
 		return {
 			express: express,
 			app: this.app,
@@ -49,47 +49,47 @@ module.exports = {
 			}
 		}
 	},
-	
+
 	setupApp: function() {
-		
+
 		// setup express app
 		this.app = express();
-		
+
 		// setup http server on app
 		this.httpServer = require('http').Server(this.app);
-		
+
 		// listen to port stated in app.port config (usually port 1337)
 		this.httpServer.listen(FormideOS.config.get('app.port'), function() {
 			FormideOS.log.info('server running on port ' + this.httpServer.address().port);
 		}.bind(this));
-		
+
 		// use json body parser for json post requests
 		this.app.use(bodyParser.json());
-		
+
 		// use json body parser for url encoded post requets
 		this.app.use(bodyParser.urlencoded({ extended: true }));
-		
+
 		// use cookie parser middleware
 		this.app.use(cookieParser());
-		
+
 		// use session middleware
 		this.app.use(sessionMiddleware);
-		
+
 		// use passport middleware
 		this.app.use(passport.initialize());
 		this.app.use(passport.session());
-		
+
 		// use bearer token middleware
 		this.app.use(bearerToken({
 			queryKey: 'access_token'
 		}));
-		
+
 		// use cors middleware
 		this.app.use(cors({
 			origin: true,
 			credentials: true
 		}));
-		
+
 		// use response handlers
 		this.app.use(function (req, res, next) {
 			res.ok = require('./responses/ok').bind({ req: req, res: res });
@@ -103,12 +103,12 @@ module.exports = {
 			next();
 		});
 	},
-	
+
 	setupPassport: function() {
 		passport.serializeUser(function(user, done) {
 		  	done(null, user.id);
 		});
-		
+
 		passport.deserializeUser(function(id, done) {
 		  	FormideOS.db.User.findOne({ id: id }).exec(function(err, user) {
 			  	if (err) return done('user not found', false);
@@ -117,15 +117,15 @@ module.exports = {
 				}
 			});
 		});
-		
+
 		passport.use('local-login', new LocalStrategy({ usernameField: 'email' }, function(email, password, next) {
-			
+
 			FormideOS.log("Attempt login for " + email);
-			
+
 			FormideOS.db.User.findOne({ email: email}, function (err, user) {
 				if (err) return next(err);
 				if (!user) return next(null, false, { message: "Incorrect credentials" });
-				
+
 				bcrypt.compare(password, user.password, function(err, isMatch) {
 					if(err) return next(err);
 					next(null, isMatch ? user : null)
