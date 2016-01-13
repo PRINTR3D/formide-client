d89d7c9db77e439f5c68fad49021196ab955ccf5
ChrisTerBeke <ctbeke@gmail.com>
2016-01-13 15:23:26 +0100

    log whole body

--- core/modules/cloud/api.js
+++ core/modules/cloud/api.js
@@ -64,7 +64,7 @@ module.exports = (routes, module) => {
 				(err, response, body) => {
 					if (err) return FormideOS.log(err.message);
 					if (response.statusCode == 200) return FormideOS.log('Device registered');
-					return FormideOS.log('Device registration failed: ' + body.message);
+					return FormideOS.log('Device registration failed: ' + body);
 				});
 		});
 	});
