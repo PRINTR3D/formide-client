5454b1a39aa106a5f7728db502e707a6ae2caa88
ChrisTerBeke <ctbeke@gmail.com>
2016-01-14 14:11:48 +0100

    change boot order

--- bootstrap.js
+++ bootstrap.js
@@ -32,13 +32,13 @@ initFormide().then(() => {
 	FormideOS.log.info('==============================================');
 
 	// Load core modules
-	FormideOS.moduleManager.loadModule('/core/modules/db',		'db',           true);
-	FormideOS.moduleManager.loadModule('/core/modules/auth',	'auth',         true);
-	FormideOS.moduleManager.loadModule('/core/modules/files', 	'files',        true);
-	FormideOS.moduleManager.loadModule('/core/modules/update',	'update',	true);
-	FormideOS.moduleManager.loadModule('/core/modules/cloud',       'cloud',	true);
-	FormideOS.moduleManager.loadModule('/core/modules/printer', 	'printer',      true);
+	FormideOS.moduleManager.loadModule('/core/modules/db',		'db',       true);
+	FormideOS.moduleManager.loadModule('/core/modules/auth',	'auth',     true);
+	FormideOS.moduleManager.loadModule('/core/modules/files', 	'files',    true);
+	FormideOS.moduleManager.loadModule('/core/modules/printer', 'printer',  true);
 	FormideOS.moduleManager.loadModule('/core/modules/slicer',	'slicer',	true);
+	FormideOS.moduleManager.loadModule('/core/modules/update',	'update',	true);
+	FormideOS.moduleManager.loadModule('/core/modules/cloud',   'cloud',	true);
 
 	// Load all via npm installed formide-client modules
 	for(var i in pkg.dependencies) {
