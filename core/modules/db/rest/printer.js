47b6549316efdec87d480f80ae70da6b54315088
ChrisTerBeke <chris@printr.com>
2016-06-30 16:51:36 +0200

    Feature/cleanup (#230)
    * remove cura upload from client
    
    * cleanup sliceprofile controller
    
    * more cleanup
    
    * add abilities to printer model
    
    * more small fixes
    
    * renable printer
    
    * update interface branch
    
    * use master interface

--- core/modules/db/rest/printer.js
+++ core/modules/db/rest/printer.js
@@ -10,10 +10,10 @@ module.exports = (routes, db) => {
 	 */
 	routes.get('/printers', (req, res) => {
 		db.Printer
-		.find({}, { select: ((req.query.fields) ? req.query.fields.split(',') : "") })
-		.sort('presetOrder ASC')
-		.then(res.ok)
-		.error(res.serverError);
+			.find({}, { select: ((req.query.fields) ? req.query.fields.split(',') : "") })
+			.sort('presetOrder ASC')
+			.then(res.ok)
+			.error(res.serverError);
 	});
 
 	/**
@@ -21,12 +21,12 @@ module.exports = (routes, db) => {
 	 */
 	routes.get('/printers/:id', (req, res) => {
 		db.Printer
-		.findOne({ id: req.params.id })
-		.then((printer) => {
-			if (!printer) return res.notFound();
-			return res.ok(printer);
-		})
-		.error(res.serverError);
+			.findOne({ id: req.params.id })
+			.then((printer) => {
+				if (!printer) return res.notFound();
+				return res.ok(printer);
+			})
+			.error(res.serverError);
 	});
 
 	/**
@@ -34,22 +34,23 @@ module.exports = (routes, db) => {
 	 */
 	routes.post('/printers', (req, res) => {
 		db.Printer
-		.create({
-			name:				req.body.name,
-			bed:				req.body.bed,
-			axis:				req.body.axis,
-			extruders:			req.body.extruders,
-			port:				req.body.port,
-			baudrate:			req.body.baudrate,
-			gcodeFlavour:		req.body.gcodeFlavour,
-			startGcode:			req.body.startGcode,
-			endGcode:			req.body.endGcode,
-			createdBy:			req.user.id
-		})
-		.then((printer) => {
-			return res.ok({ message: "Printer created", printer });
-		})
-		.error(res.serverError);
+			.create({
+				name:				req.body.name,
+				bed:				req.body.bed,
+				axis:				req.body.axis,
+				extruders:			req.body.extruders,
+				port:				req.body.port,
+				baudrate:			req.body.baudrate,
+				gcodeFlavour:		req.body.gcodeFlavour,
+				startGcode:			req.body.startGcode,
+				endGcode:			req.body.endGcode,
+				abilities:          req.body.abilities,
+				createdBy:			req.user.id
+			})
+			.then((printer) => {
+				return res.ok({ message: "Printer created", printer });
+			})
+			.error(res.serverError);
 	});
 
 	/**
@@ -57,22 +58,22 @@ module.exports = (routes, db) => {
 	 */
 	routes.put('/printers/:id', (req, res) => {
 		db.Printer
-		.update({ id: req.params.id }, {
-			name:				req.body.name,
-			bed:				req.body.bed,
-			axis:				req.body.axis,
-			extruders:			req.body.extruders,
-			port:				req.body.port,
-			baudrate:			req.body.baudrate,
-			gcodeFlavour:		req.body.gcodeFlavour,
-			startGcode:			req.body.startGcode,
-			endGcode:			req.body.endGcode,
-			updatedBy:			req.user.id
-		})
-		.then((updated) => {
-			return res.ok({ message: "Printer updated", printer: updated[0] });
-		})
-		.error(res.serverError);
+			.update({ id: req.params.id }, {
+				name:				req.body.name,
+				bed:				req.body.bed,
+				axis:				req.body.axis,
+				extruders:			req.body.extruders,
+				port:				req.body.port,
+				baudrate:			req.body.baudrate,
+				gcodeFlavour:		req.body.gcodeFlavour,
+				startGcode:			req.body.startGcode,
+				endGcode:			req.body.endGcode,
+				updatedBy:			req.user.id
+			})
+			.then((updated) => {
+				return res.ok({ message: "Printer updated", printer: updated[0] });
+			})
+			.error(res.serverError);
 	});
 
 	/**
@@ -80,10 +81,10 @@ module.exports = (routes, db) => {
 	 */
 	routes.delete('/printers/:id', (req, res) => {
 		db.Printer
-		.destroy({ id: req.params.id, preset: false })
-		.then(() => {
-			return res.ok({ message: "Printer deleted" });
-		})
-		.error(res.serverError);
+			.destroy({ id: req.params.id, preset: false })
+			.then(() => {
+				return res.ok({ message: "Printer deleted" });
+			})
+			.error(res.serverError);
 	});
 };
--- core/modules/db/rest/userfile.js
+++ core/modules/db/rest/userfile.js
@@ -14,10 +14,10 @@ module.exports = (routes, db) => {
 	 */
 	routes.get('/files', (req, res) => {
 		db.UserFile
-		.find({}, { select: ((req.query.fields) ? req.query.fields.split(',') : "") })
-		.populate('printJobs')
-		.then(res.ok)
-		.error(res.serverError);
+			.find({ createdBy: req.user.id }, { select: ((req.query.fields) ? req.query.fields.split(',') : "") })
+			.populate('printJobs')
+			.then(res.ok)
+			.error(res.serverError);
 	});
 
 	/**
@@ -25,34 +25,34 @@ module.exports = (routes, db) => {
 	 */
 	routes.get('/files/:id', (req, res) => {
 		db.UserFile
-		.findOne({ id: req.params.id })
-		.populate('printJobs')
-		.then(userFile => {
-			if (!userFile) return res.notFound();
-			async.map(userFile.printJobs, (printjob, callback) => {
-				db.PrintJob
-				.find({ id: printjob.id, createdBy: req.user.id })
-				.populate('materials')
-				.populate('printer')
-				.populate('sliceProfile')
-				.then(printers => {
-					return callback(null, printers);
-				})
-				.error(callback);
-			}, (asyncErr, results) => {
-				if (asyncErr) return res.serverError(asyncErr);
-				var response = [];
-				for (var i in results) {
-					for (var j in results[i]) {
-						response.push(results[i][j]);
+			.findOne({ id: req.params.id })
+			.populate('printJobs')
+			.then(userFile => {
+				if (!userFile) return res.notFound();
+				async.map(userFile.printJobs, (printjob, callback) => {
+					db.PrintJob
+					.find({ id: printjob.id, createdBy: req.user.id })
+					.populate('materials')
+					.populate('printer')
+					.populate('sliceProfile')
+					.then(printers => {
+						return callback(null, printers);
+					})
+					.error(callback);
+				}, (asyncErr, results) => {
+					if (asyncErr) return res.serverError(asyncErr);
+					var response = [];
+					for (var i in results) {
+						for (var j in results[i]) {
+							response.push(results[i][j]);
+						}
 					}
-				}
-				userFile = userFile.toJSON();
-				userFile.printJobs = response;
-				return res.ok(userFile);
-			});
-		})
-		.error(res.serverError);
+					userFile = userFile.toJSON();
+					userFile.printJobs = response;
+					return res.ok(userFile);
+				});
+			})
+			.error(res.serverError);
 	});
 
 	/**
@@ -60,13 +60,13 @@ module.exports = (routes, db) => {
 	 */
 	routes.post('/files/:id', function(req, res) {
 		db.UserFile
-		.update({ id: req.params.id, createdBy: req.user.id }, {
-			prettyname:	req.body.prettyname
-		})
-		.then((updated) => {
-			return res.ok({ message: "File updated", file: updated[0] })
-		})
-		.error(res.serverError);
+			.update({ id: req.params.id, createdBy: req.user.id }, {
+				prettyname:	req.body.prettyname
+			})
+			.then((updated) => {
+				return res.ok({ message: "File updated", file: updated[0] })
+			})
+			.error(res.serverError);
 	});
 
 	/**
@@ -74,23 +74,23 @@ module.exports = (routes, db) => {
 	 */
 	routes.delete('/files/:id', function(req, res) {
 		db.UserFile
-		.findOne({ id: req.params.id, createdBy: req.user.id })
-		.then(userFile => {
-			// delete file from storage
-			var filePath = path.join(FormideOS.config.get('app.storageDir'), FormideOS.config.get('paths.modelfiles'), userFile.hash);
-			try {
-				fs.unlinkSync(filePath);
-			}
-			catch (e) {
-				FormideOS.log.warn('file could not be deleted from storage');
-			}
+			.findOne({ id: req.params.id, createdBy: req.user.id })
+			.then(userFile => {
+				// delete file from storage
+				var filePath = path.join(FormideOS.config.get('app.storageDir'), FormideOS.config.get('paths.modelfiles'), userFile.hash);
+				try {
+					fs.unlinkSync(filePath);
+				}
+				catch (e) {
+					FormideOS.log.warn('file could not be deleted from storage');
+				}
 
-			// delete from database
-			userFile.destroy(function (err) {
-				if (err) return res.serverError(err);
-				return res.ok({ message: "File deleted" });
-			});
-		})
-		.catch(res.serverError);
+				// delete from database
+				userFile.destroy(function (err) {
+					if (err) return res.serverError(err);
+					return res.ok({ message: "File deleted" });
+				});
+			})
+			.catch(res.serverError);
 	});
 };
