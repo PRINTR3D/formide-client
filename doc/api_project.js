9e987d2e0368c1a02ce8b8c3193c89b8c6a88f2f
Chris ter Beke <ctbeke@gmail.com>
2016-09-05 23:22:01 +0200

    add docs

---
+++ doc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "formide-client",  "version": "1.3.6",  "description": "Formide Client API",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-09-05T21:21:43.499Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
