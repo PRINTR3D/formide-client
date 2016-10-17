1f3d223a4fcced7b0be6e3be1a8186dbd20a2bc5
Chris ter Beke <ctbeke@gmail.com>
2016-10-17 21:48:09 +0200

    code cleanup

--- src/utils/db/seed.js
+++ src/utils/db/seed.js
@@ -7,78 +7,62 @@ const uuid    = require('node-uuid');
 const thenify = require('thenify');
 const readdir = thenify(fs.readdir);
 
-module.exports = (db, presetStorage) => { co(function*() {
+module.exports = (db, presetStorage) => {
+    co(function*() {
 
-    // static seeds
-    yield db.User.findOrCreate({
-        email:    "admin@local",
-        isAdmin:  true
-    }, {
-        email:    "admin@local",
-        password: "admin",
-        isAdmin:  true
-    });
+        // static seeds
+        yield db.User.findOrCreate({
+            email:    "admin@local",
+            isAdmin:  true
+        }, {
+            email:    "admin@local",
+            password: "admin",
+            isAdmin:  true
+        });
 
-    // TODO: actually, the whole database needs to be replaced with something better
+        // TODO: actually, the whole database needs to be replaced with something better
 
-    // material seeds
-    readdir(path.join(presetStorage, './materials')).then((files) => {
-        files.forEach(file => { co(function*() {
-            if (file.match(/\.json$/) == null) return;
-            const preset = require(path.join(presetStorage, './materials', file));
-            preset.preset = true;
-            yield db.Material.destroy({ preset: true });
-            yield db.Material.create(preset);
-        }).then(null, console.error); });
-    });
+        // material seeds
+        readdir(path.join(presetStorage, './materials'))
+            .then(function (files) {
+                files.forEach(function(file) {
+                    co(function*() {
+                        if (file.match(/\.json$/) == null) return;
+                        const preset = require(path.join(presetStorage, './materials', file));
+                        preset.preset = true;
+                        yield db.Material.destroy({ preset: true });
+                        yield db.Material.create(preset);
+                    }).then(null, console.error);
+                });
+            });
 
-    // sliceprofile seeds
-    readdir(path.join(presetStorage, './sliceprofiles')).then((files) => {
-        files.forEach(file => { co(function*() {
-            if (file.match(/\.json$/) == null) return;
-            const preset = require(path.join(presetStorage, './sliceprofiles', file));
-            preset.preset = true;
-            yield db.SliceProfile.destroy({ preset: true });
-            yield db.SliceProfile.create(preset);
-        }).then(null, console.error); });
-    });
+        // sliceprofile seeds
+        readdir(path.join(presetStorage, './sliceprofiles'))
+            .then(function (files) {
+                return files.forEach(function(file) {
+                    co(function*() {
+                        if (file.match(/\.json$/) == null) return;
+                        const preset = require(path.join(presetStorage, './sliceprofiles', file));
+                        preset.preset = true;
+                        yield db.SliceProfile.destroy({ preset: true });
+                        yield db.SliceProfile.create(preset);
+                    }).then(null, console.error);
+                });
+            });
 
-    // printer seeds
-    readdir(path.join(presetStorage, './printers')).then((files) => {
-        files.forEach(file => { co(function*() {
-            if (file.match(/\.json$/) == null) return;
-            const preset = require(path.join(presetStorage, './printers', file));
-            preset.preset = true;
-            yield db.Printer.destroy({ preset: true });
-            yield db.Printer.create(preset);
-        }).then(null, console.error); });
-    });
+        // printer seeds
+        readdir(path.join(presetStorage, './printers'))
+            .then(function (files) {
+                files.forEach(function (file) {
+                    co(function*() {
+                        if (file.match(/\.json$/) == null) return;
+                        const preset = require(path.join(presetStorage, './printers', file));
+                        preset.preset = true;
+                        yield db.Printer.destroy({ preset: true });
+                        yield db.Printer.create(preset);
+                    }).then(null, console.error);
+                });
+            });
 
-    // model seeds
-    // readdir(path.join(presetStorage, './modelfiles')).then((files) => {
-    //     files.forEach(file => { co(function*() {
-    //         if (file.match(/\.stl/) == null && file.match(/\.gcode/) == null) return;
-    //
-    //         const filePath = path.join(presetStorage, './modelfiles', file);
-    //         const ext = path.extname(filePath).toLowerCase();
-    //         const hash = uuid.v4();
-    //
-    //         // insert in database
-    //         const userFile = yield db.UserFile.findOrCreate({
-    //             filename: file
-    //         }, {
-    //             prettyname: file,
-    //             filename:   file,
-    //             filesize:   0, // TODO
-    //             filetype:   `text/${ext.replace('.', '')}`,
-    //             hash:       hash
-    //         });
-    //
-    //         // copy the file (or overwrite when already in DB)
-    //         const newPath = path.join(FormideClient.config.get('app.storageDir'), FormideClient.config.get('paths.modelfiles'), userFile.hash);
-    //         fs.createReadStream(filePath).pipe(fs.createWriteStream(newPath));
-    //
-    //     }).then(null, console.error); });
-    // });
-
-}).then(null, console.error); };
+    }).then(null, console.error);
+};
