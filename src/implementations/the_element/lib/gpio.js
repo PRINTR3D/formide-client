12c9b8b44aaeeda5cd60457949aae413e6bc8c5e
Chris ter Beke <ctbeke@gmail.com>
2016-10-28 19:04:30 +0200

    start gpio lib for usb switching

---
+++ src/implementations/the_element/lib/gpio.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * This module uses the GPIO lib available on The Element in the Felix Pro 2 to switch control
+ * between The Element and external USB host.
+ */
+
+const GPIO_STATUS = '';
+const GPIO_SWITCH = '';
+
+module.exports = {
+
+    registerOnChange(callback) {
+        // TODO: emit to callback when USB host status changes
+    },
+
+    switchControlMode(mode, callback) {
+        // TODO: call GPIO lib to change control mode
+    }
+}
