053d981636b399df230a35493e867bffe5a0295e
Chris ter Beke <ctbeke@gmail.com>
2016-10-29 17:01:41 +0200

    remove deps

--- src/implementations/the_element/index.js
+++ src/implementations/the_element/index.js
@@ -1,12 +1,5 @@
 'use strict';
 
-const fs   = require('fs');
-const uuid = require('uuid');
-
-const Handlebars = require('handlebars');
-Handlebars.registerHelper('list',
-    (ctx, opt) => ctx.reduce((prev, curr) => prev + opt.fn(curr), ''));
-
 const wifi   = require('./lib/wifi');
 const update = require('./lib/update');
 const usb    = require('./lib/usb');
