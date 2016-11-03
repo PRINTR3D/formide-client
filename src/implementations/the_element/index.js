9b72f926e7dc86f391ee78c5160da3e8698a1bdc
Chris ter Beke <ctbeke@gmail.com>
2016-11-03 16:23:04 +0100

    enable gpio

--- src/implementations/the_element/index.js
+++ src/implementations/the_element/index.js
@@ -3,8 +3,8 @@
 const wifi   = require('./lib/wifi');
 const update = require('./lib/update');
 const usb    = require('./lib/usb');
-// const gpio   = require('./lib/gpio');
+const gpio   = require('./lib/gpio');
 
 module.exports = {
-	wifi, update, usb
+	wifi, update, usb, gpio
 };
