5128d86d4e60c5ac19e6f3d996365e8218cf1f2a
Chris ter Beke <ctbeke@gmail.com>
2016-11-17 09:54:15 +0100

    escape config before passing in fiw

--- src/implementations/the_element/lib/wifi.js
+++ src/implementations/the_element/lib/wifi.js
@@ -187,8 +187,10 @@ function connectAdvanced(config, callback) {
         wpa_supplicant += `${param}="${config[param]}"\n`;
     wpa_supplicant += '}';
 
+    const _wpa_supplicant = bashEscape(wpa_supplicant);
+
     // execute reconnect of fiw service, now using updated config
-    exec(`${service} wlan0 connectWithConfig ${wpa_supplicant}`, (err, stdout) => {
+    exec(`${service} wlan0 connectWithConfig ${_wpa_supplicant}`, (err, stdout) => {
         if (err)
             return callback(err);
 
